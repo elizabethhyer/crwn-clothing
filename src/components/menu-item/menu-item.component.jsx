11354b8b09ef0e1a4dbb790dd863856a7d96d7f6
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-12-05 12:38:35 -0500

    update our menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+//withRouter is higher order component (a function that takes a component as an argument, and returns back to you a modified component.)
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
@@ -15,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     </div>
   );
 };
-export default MenuItem;
+export default withRouter(MenuItem); //now that we've sent the component into withRouter, we have access to history
