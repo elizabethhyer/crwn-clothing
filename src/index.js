f6f8b70506e7e54282b5d2958ce6169c05723275
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-12-08 19:21:38 -0500

    added redux and implemented userReducer and userActions to header and app components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
+
+//Provider is the parent component of EVERYTHING else in the application, so it wraps the entire app - it gives the store context to the rest of the application
