4dcf1c130819afdadae91be1a110d3210adc0fbc
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-12-16 19:35:21 -0500

    moved our directory and shop data into their own reducers, created respective selectors and updated corresponding componenets with new redux flow for directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,7 +7,6 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log("WHYYYYYY");
   const { title, items } = collection;
   return (
     <div className="collection-page">
